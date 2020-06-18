6a579f1853a3b275d7d24e2f0bae69757a138ac9
Sean Koniarz <sean.koniarz@pm.me>
2020-06-18 11:40:16 -0400

    initial deploy

---
+++ bin/sysmonupdate.ps1
@@ -0,0 +1,40 @@
+# Set some variables
+$Logfile = "c:\temp\sysmondeploy.log"
+$SystemSysmonConfig = 'C:\windows\sysmonconfig-export.xml'
+$SysmonLocation = 'c:\windows\'
+$SplunkSysmonConfig = 'C:\Program Files\SplunkUniversalForwarder\etc\apps\TA-Sysmon-deploy\bin\sysmonconfig-export.xml'
+$sysmonexe = '..\bin\Sysmon.exe'
+
+Start-Transcript -Path $Logfile
+function CheckInstallStatus($software)
+{
+    # Function to check if software is installed on system
+    $softwareinstalled = (Get-Service -DisplayName $software | Where { $_.DisplayName -match $software }) -ne $null
+    return $softwareinstalled 
+}
+function CheckSysmonVersion()
+{
+    $versioninfo = [System.Diagnostics.FileVersionInfo]::GetVersionInfo("C:\WINDOWS\Sysmon.exe").FileVersion
+    return $versioninfo
+}
+$sysmoninstalled = CheckInstallStatus "Sysmon"
+if(-not $sysmoninstalled)
+{
+    Write-Host (Get-Date -Format "MM/dd/yyyy HH:mm K") 'sysmon needs to be installed, installing...'
+    Write-Host (Get-Date -Format "MM/dd/yyyy HH:mm K") 'copying new config file to c:\windows'
+    Copy-Item $SplunkSysmonConfig -Destination $SysmonLocation
+    ..\bin\Sysmon.exe -accepteula -i $SystemSysmonConfig
+}
+else {
+    $sysmonversion = CheckSysmonVersion
+    Write-Host (Get-Date -Format "MM/dd/yyyy HH:mm K") 'sysmon is installed version:'$sysmonversion
+    Write-Host (Get-Date -Format "MM/dd/yyyy HH:mm K") 'checking if config needs updated'
+    if ($(Get-FileHash $SystemSysmonConfig).Hash -ne $(Get-FileHash $SplunkSysmonConfig).Hash){
+        Write-Host (Get-Date -Format "MM/dd/yyyy HH:mm K") 'config needs updated, updating...'
+        Write-Host (Get-Date -Format "MM/dd/yyyy HH:mm K") 'copying new config file to c:\windows'
+        Copy-Item $SplunkSysmonConfig -Destination $SysmonLocation
+        ..\bin\Sysmon.exe -c $SplunkSysmonConfig
+    } 
+    Write-Host (Get-Date -Format "MM/dd/yyyy HH:mm K") 'Config up to date and sysmon installed'
+}
+Stop-Transcript 
