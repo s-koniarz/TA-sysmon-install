38d5b8c7c1853b6f19f2e4bc477ee1e1371efbeb
Sean Koniarz <sean.koniarz@pm.me>
2020-06-18 13:42:22 -0400

    add in updating to powershell and checking if config exists in proper location

--- bin/sysmonupdate.ps1
+++ bin/sysmonupdate.ps1
@@ -2,7 +2,9 @@
 $Logfile = "c:\temp\sysmondeploy.log"
 $SystemSysmonConfig = 'C:\windows\sysmonconfig-export.xml'
 $SysmonLocation = 'c:\windows\'
-$SplunkSysmonConfig = 'C:\Program Files\SplunkUniversalForwarder\etc\apps\TA-Sysmon-deploy\bin\sysmonconfig-export.xml'
+$SplunkSysmonConfig = 'C:\Program Files\SplunkUniversalForwarder\etc\apps\TA-sysmon-install\bin\sysmonconfig-export.xml'
+$sysmoninstalllocation = 'C:\Windows\Sysmon.exe'
+$Splunksysmonlocation = 'C:\Program Files\SplunkUniversalForwarder\etc\apps\TA-sysmon-install\bin\Sysmon.exe'
 
 Start-Transcript -Path $Logfile
 function CheckInstallStatus($software)
@@ -11,29 +13,47 @@ function CheckInstallStatus($software)
     $softwareinstalled = (Get-Service -DisplayName $software | Where-Object { $_.DisplayName -match $software })  -ne $null
     return $softwareinstalled 
 }
-function CheckSysmonVersion()
+function CheckSysmonVersion($path)
 {
-    $versioninfo = [System.Diagnostics.FileVersionInfo]::GetVersionInfo("C:\WINDOWS\Sysmon.exe").FileVersion
+    $versioninfo = [System.Diagnostics.FileVersionInfo]::GetVersionInfo($path).FileVersion
     return $versioninfo
 }
+
+# Check if sysmon is insallted Variable setting
 $sysmoninstalled = CheckInstallStatus "Sysmon"
+$sysmonlocal = (CheckSysmonVersion($Splunksysmonlocation))
+$sysmonsplunk = (CheckSysmonVersion($sysmoninstalllocation))
 if(-not $sysmoninstalled)
 {
     Write-Host (Get-Date -Format "MM/dd/yyyy HH:mm K") 'sysmon needs to be installed, installing...'
     Write-Host (Get-Date -Format "MM/dd/yyyy HH:mm K") 'copying new config file to c:\windows'
     Copy-Item $SplunkSysmonConfig -Destination $SysmonLocation
     ..\bin\Sysmon.exe -accepteula -i $SystemSysmonConfig
-}
-else {
-    $sysmonversion = CheckSysmonVersion
-    Write-Host (Get-Date -Format "MM/dd/yyyy HH:mm K") 'sysmon is installed version:'$sysmonversion
+}elseif($sysmonlocal -ne $sysmonsplunk) {
+    Write-Host (Get-Date -Format "MM/dd/yyyy HH:mm K") 'sysmon out of date, updating and applying config'
+    Write-Host (Get-Date -Format "MM/dd/yyyy HH:mm K") 'uninstalling sysmon'
+    ..\bin\Sysmon.exe -u 
+    Write-Host (Get-Date -Format "MM/dd/yyyy HH:mm K") 'copying newest config to c:\windows'
+    Copy-Item $SplunkSysmonConfig -Destination $SysmonLocation
+    Write-Host (Get-Date -Format "MM/dd/yyyy HH:mm K") 'installing newest version of sysmon'
+    ..\bin\Sysmon.exe -accepteula -i $SystemSysmonConfig
+}else{
+    $sysmoninstalledversion = CheckSysmonVersion($sysmoninstalllocation)
+    Write-Host (Get-Date -Format "MM/dd/yyyy HH:mm K") 'sysmon is installed version:'$sysmoninstalledversion
     Write-Host (Get-Date -Format "MM/dd/yyyy HH:mm K") 'checking if config needs updated'
-    if ($(Get-FileHash $SystemSysmonConfig).Hash -ne $(Get-FileHash $SplunkSysmonConfig).Hash){
-        Write-Host (Get-Date -Format "MM/dd/yyyy HH:mm K") 'config needs updated, updating...'
-        Write-Host (Get-Date -Format "MM/dd/yyyy HH:mm K") 'copying new config file to c:\windows'
+    if (Test-Path -LiteralPath "C:\Windows\sysmonconfig-export.xml"){
+        if ($(Get-FileHash $SystemSysmonConfig).Hash -ne $(Get-FileHash $SplunkSysmonConfig).Hash){
+            Write-Host (Get-Date -Format "MM/dd/yyyy HH:mm K") 'config needs updated, updating...'
+            Write-Host (Get-Date -Format "MM/dd/yyyy HH:mm K") 'copying new config file to c:\windows'
+            Copy-Item $SplunkSysmonConfig -Destination $SysmonLocation
+            ..\bin\Sysmon.exe -c $SplunkSysmonConfig
+        } 
+    }else{
+        Write-Host (Get-Date -Format "MM/dd/yyyy HH:mm K") 'sysmon installed with no config present in c:\windows'
+        Write-Host (Get-Date -Format "MM/dd/yyyy HH:mm K") 'copying new config and applying'
         Copy-Item $SplunkSysmonConfig -Destination $SysmonLocation
         ..\bin\Sysmon.exe -c $SplunkSysmonConfig
-    } 
-    Write-Host (Get-Date -Format "MM/dd/yyyy HH:mm K") 'Config up to date and sysmon installed'
+    }
+    Write-Host (Get-Date -Format "MM/dd/yyyy HH:mm K") 'Config up to date and sysmon installed at proper version'
 }
 Stop-Transcript 
